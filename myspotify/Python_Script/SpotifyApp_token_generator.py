74d2ee2a18921908fc52cd08fb9e0b20fbe7d518
yiliyu1211 <td821211@gmail.com>
2020-07-04 01:49:04 -0500

    Update SpotifyApp_token_generator.py

--- myspotify/Python_Script/SpotifyApp_token_generator.py
+++ myspotify/Python_Script/SpotifyApp_token_generator.py
@@ -9,11 +9,11 @@ Created on Sat Jul  4 01:28:12 2020
 
 import spotipy.util as util
 
-username = '9z2uftm6g5wisylxrqzc056f0' 
+username = 'your user name' 
 # your username (find in Spotify account)
 
-client_id ='dc49b39997f14cd792e856d4e4fbb7d3' 
-client_secret = '1972ad083a954d35bd98b55430a99797' 
+client_id ='your client id' 
+client_secret = 'your client secret' 
 # client id and client secret (find in Spotify app account)
 
 redirect_uri = 'http://localhost:7777/callback'
